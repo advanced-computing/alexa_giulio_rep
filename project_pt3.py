edadb4be98b54e419622c37227844c36cc8b3c27
asc2244 <asc2244@columbia.edu>
2025-02-24 15:43:09 -0500

    created data cleaning function and unit tested it

---
+++ project_pt3.py
@@ -0,0 +1,15 @@
+
+#creating a general function to clean the artist name column
+import pandas as pd
+import spotify
+
+#separates out multiple artists that are credited on a single song
+def name_cleaning(df: pd.DataFrame, column_name: str):
+    df[column_name] = df[column_name].str.split(", ")
+    return name_separating(df, column_name)
+
+
+#puts the individual artists into their own rows as a list
+def name_separating(df: pd.DataFrame, column_name: str):
+    df = df.explode(column_name)[column_name].tolist()
+    return df
