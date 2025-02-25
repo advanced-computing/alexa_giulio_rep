c33d0dd63844edcae26c59655eaf414158b033f7
GiulioTrombin <gt2503@columbia.edu>
2025-02-25 15:47:48 -0500

    Added latest  giulio_test.py and giulio_functions_test.py

--- giulio_test.py
+++ giulio_test.py
@@ -57,4 +57,4 @@ def test_load_and_prepare_data_correct_countries():
     # Check if the countries are as expected
     assert countries == expected_countries
 
-#python -m pytest GIULIO_TEST.py
+#python -m pytest giulio_test.py
