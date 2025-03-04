4f5436409e476aa75b4271df5d2911557cbb0206
asc2244 <asc2244@columbia.edu>
2025-03-03 22:24:51 -0500

    using a fix to try to get around ruff

--- pages/Project_Part_4:_Spotify_Dashboard.py
+++ pages/Project_Part_4:_Spotify_Dashboard.py
@@ -2,11 +2,13 @@ import streamlit as st
 import plotly.express as px
 import sys
 import os
+
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 utils_path = os.path.join(project_root, "utils")
 if utils_path not in sys.path:
     sys.path.append(utils_path)
-from helper_functions_notebook import call_api, rain_emojis
+
+from helper_functions_notebook import call_api, rain_emojis  # noqa: E402
 
 #loading dataset
 dataset_path = 'asaniczka/top-spotify-songs-in-73-countries-daily-updated'
