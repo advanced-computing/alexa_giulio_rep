c2897676f6a64d0565529a79e932a71b930fcf35
asc2244 <asc2244@columbia.edu>
2025-03-03 22:13:33 -0500

    does ruff still hate me

--- pages/Project_Part_4:_Spotify_Dashboard.py
+++ pages/Project_Part_4:_Spotify_Dashboard.py
@@ -1,17 +1,10 @@
 import sys
 import os
-
-# Get the absolute path of the project root (one level up from pages/)
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 utils_path = os.path.join(project_root, "utils")
-
-# Add utils directory to sys.path if not already included
 if utils_path not in sys.path:
     sys.path.append(utils_path)
-
-# Now, import the module
 from helper_functions_notebook import call_api, rain_emojis
-
 import streamlit as st
 import plotly.express as px
 
