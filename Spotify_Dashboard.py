5b958be171fa9713e58189fdaaaf1437518223d0
asc2244 <asc2244@columbia.edu>
2025-03-25 19:40:11 -0400

    Initial commit of Streamlit app

--- Spotify_Dashboard.py
+++ Spotify_Dashboard.py
@@ -55,6 +55,23 @@ if not new_data.empty:
 else:
     print("no new records to upload.")
 
+# get GCP credentials from secrets
+from google.oauth2 import service_account
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+# use credentials to query
+PROJECT_ID = st.secrets["gcp_service_account"]["project_id"]
+DATASET = "spotify"
+TABLE = "universal_top_spotify_songs"
+FULL_TABLE_ID = f"{PROJECT_ID}.{DATASET}.{TABLE}"
+
+query = f"SELECT * FROM `{FULL_TABLE_ID}` LIMIT 10"
+df = pandas_gbq.read_gbq(query, project_id=PROJECT_ID, credentials=credentials)
+
+st.write(df)
+
 #name column cleaning
 spotify_data["artists"] = spotify_data["artists"].str.split(", ")
 spotify_data2 = spotify_data.explode("artists")
